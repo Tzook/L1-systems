7f0ecdca05191eaddc4a028603afd66b37bd8511
NoamElb <noam.elboim@gmail.com>
2015-02-23 14:06:24 +0200

    typo fix

--- app/scripts/controllers/landing.js
+++ app/scripts/controllers/landing.js
@@ -6,7 +6,7 @@
 	angular.module('landing', [])
 	.controller('LandingCtrl', ['$scope', 'Head', function($scope, Head) {
 		Head.setHead("Integration Solutions",
-					"L1-Systems provide creative integration solutions using cutting-edge technologies of various kinds, providing to varios nation-level entities.",
+					"L1-Systems provide creative integration solutions using cutting-edge technologies of various kinds, providing to various nation-level entities.",
 					"Integration, Solutions, Technologies, Markets, Intelligence, Aviation, Communication, Safe Cities, Federal Assets, Airports, Borders");
 	}]);
 })(angular);
